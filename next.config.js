7fdc818a76bce40b1dc7b9a81743e68ce440be25
MFNgroup <ndiayeledaf@gmail.com>
2026-03-31 10:17:03 +0000

    Remove mysql2 from serverExternalPackages
    Removed 'mysql2' from serverExternalPackages.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
 
   // Nécessaire pour que mysql2 fonctionne côté serveur sur Vercel
-  serverExternalPackages: ['mysql2'],
+  
 
   env: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
